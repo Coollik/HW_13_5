6f523362c3af6cf514d549fd42c302c5963ebb03
Aleksey Kulikov <kulikov.alx@yandex.ru>
2025-05-20 20:43:02 +0400

    импортирован selene

--- test/conftest.py
+++ test/conftest.py
@@ -1,1 +1,8 @@
 import pytest
+from selene import browser
+
+@pytest.fixture(scope='function', autouse=True)
+
+def browser_settings():
+    browser.config.type_by_js=True
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
